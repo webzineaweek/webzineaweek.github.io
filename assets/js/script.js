3d6c7afda9950e5d2bf8ffd03c421f660c69c332
webzineaweek <76433628+webzineaweek@users.noreply.github.com>
2021-01-09 17:40:43 +0900

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,37 @@
 $(document).ready(function(){
 
 	// $('body').scrollTop(0);
-
 	setTimeout(function() {
-		$('#loding5').css('background-color', '#39FF47');
+		$('#loding6').css('background-color', '#39FF47');
 	}, 1000);
 
 	setTimeout(function() {
-		$('#loding4').css('background-color', '#39FF47');
+		$('#loding5').css('background-color', '#39FF47');
 	}, 1500);
 
 	setTimeout(function() {
-		$('#loding3').css('background-color', '#39FF47');
+		$('#loding4').css('background-color', '#39FF47');
 	}, 2000);
 
 	setTimeout(function() {
-		$('#loding2').css('background-color', '#39FF47');
+		$('#loding3').css('background-color', '#39FF47');
 	}, 2500);
 
 	setTimeout(function() {
-		$('#loding1').css('background-color', '#39FF47');
+		$('#loding2').css('background-color', '#39FF47');
 	}, 3000);
 
 	setTimeout(function() {
+		$('#loding1').css('background-color', '#39FF47');
+	}, 3500);
+
+	setTimeout(function() {
 		$('#preloader').hide();
-	}, 3600);
+	}, 4100);
 
 	setTimeout(function() {
 		$('#preloader').show();
-	}, 3800);
+	}, 4400);
 
 	setTimeout(function() {
 		$('#preloader').hide();
@@ -39,9 +42,9 @@ $(document).ready(function(){
 		});
 		$('.main').css('visibility', 'visible');
 
-	}, 4400);
+	}, 4900);
 
-	
+
 
 	$('.index-wrapper').hover(function( ) {
 		$('.info').show();
@@ -61,7 +64,7 @@ $(document).ready(function(){
 
 	$('.date-wrapper > ul > li').each(function( ) {
 		$(this).css({
-			left : Math.random() * ($('.date-wrapper').width() - $(this).width()) + 5,
+			left : Math.random() * $('.date-wrapper').width(),
 			top : Math.random() * (($('.date-wrapper').height() -10) - $(this).height()) + 20
 		});
 	});
@@ -100,7 +103,7 @@ $(document).ready(function(){
 
 	var Nov13diary 	= [];
 	Nov13diary[0] 	= '<span>주말을 앞둔 금요일은 어수선하다. 이동이 많은 날은 핸드폰 확인도 어렵다. 오랜만에 만원 버스, 만원 지하철을 탔다. 한 자리에 가만히 있는 일은 몸에 피로감을 준다. 멍해진다.</span> ';
-	Nov13diary[1] 	= '<span>뭘 하러 가는지는 가끔 잊고 가는 과정에서의 감각이 하루를 지배하기도 한다.</span> ';	
+	Nov13diary[1] 	= '<span>뭘 하러 가는지는 가끔 잊고 가는 과정에서의 감각이 하루를 지배하기도 한다.</span> ';
 	Nov13diary[2] 	= '<span>집 가는 길에 앞이 안 보일 정도로 안개가 꼈다. 정말 한 치 앞이 안 보여서 보이는 건 자동차 불빛밖에 없었다.</span> ';
 	Nov13diary[3] 	= '<span>평소엔 빠르게 지나가던 길인데 차들이 다 같이 비상등을 켜고 천천히 가는 게 뭔가 귀여워 보였다.</span> ';
 
@@ -113,28 +116,28 @@ $(document).ready(function(){
 
 	var Nov15diary 	= [];
 	Nov15diary[0] 	= '<span>인천에 산다고 하면 바다가 가깝냐고 물어보는 사람들이 있다. 나는 최근 몇년을 제외하고는 쭉 인천에 살았는데, 바다와는 먼 곳에서 살았다. </span>';
-	Nov15diary[1] 	= '<br><img class="diaryImg" src="assets/img/christmas.jpg"><img class="diaryImg" src="assets/img/christmas2.jpg"><span id="christmas">핸드폰을 보다가 어떤 사진에 멈췄다. 올해 처음 본 크리스마스 사진이다. 나는 겨울이 좋다. 겨울의 색이 좋다. 크리스마스의 색도 좋다.</span> ';	
-	Nov15diary[2] 	= '<span id="snow">눈이 오는 크리스마스를 보고 싶다. 벌써 크리스마스가 한 달 정도밖에 남지 않았다. </span>';	
+	Nov15diary[1] 	= '<br><img class="diaryImg" src="assets/img/christmas.jpg"><img class="diaryImg" src="assets/img/christmas2.jpg"><span id="christmas">핸드폰을 보다가 어떤 사진에 멈췄다. 올해 처음 본 크리스마스 사진이다. 나는 겨울이 좋다. 겨울의 색이 좋다. 크리스마스의 색도 좋다.</span> ';
+	Nov15diary[2] 	= '<span id="snow">눈이 오는 크리스마스를 보고 싶다. 벌써 크리스마스가 한 달 정도밖에 남지 않았다. </span>';
 
 	var Nov16diary 	= [];
 	Nov16diary[0] 	= '<img class="diaryImg" src="assets/img/plant.jpg"><span id="plant">집에 돌아오니 화분에 꽃이 피어있다. 집을 비운 3일간 블라인드가 올라가 있어서 빛을 많이 받은 모양이다. 전에 작업실에서 키우던 화분은 겨울에 추워서 얼어 죽었다. 그 이후로 몇 년간 식물을 키우지 않았는데 두 번째 식물은 다행히 계속 살 거 같다.</span> ';
-	Nov16diary[1] 	= '<br><span>오늘에서야 근 며칠간의 만남 일정들이 마무리되었다. 일정이 몰려있었다. 누군가를 만나는 일에 긴장하지 않아도 되는데 긴장이 된다. 어쩔 수 없는 일이다. </span>';	
+	Nov16diary[1] 	= '<br><span>오늘에서야 근 며칠간의 만남 일정들이 마무리되었다. 일정이 몰려있었다. 누군가를 만나는 일에 긴장하지 않아도 되는데 긴장이 된다. 어쩔 수 없는 일이다. </span>';
 	Nov16diary[2] 	= '<br><span>글보다 말이 좋다고 생각했는데, 점점 듣기보다 읽기가 더 편한 거 같다. (아니, 읽기도 어렵다.) 글은 선택해서 읽는 것이 가능한데 말은 그게 안 되니 참아야 한다. </span>';
-	Nov16diary[3] 	= '<br><span>(요즘은 아예 안 하지만) 게임을 할 때도 로그아웃하는 순간이 좋았다. 뭔가를 쏟아내다가 문을 닫아버리는 순간이 좋다. </span>';	
+	Nov16diary[3] 	= '<br><span>(요즘은 아예 안 하지만) 게임을 할 때도 로그아웃하는 순간이 좋았다. 뭔가를 쏟아내다가 문을 닫아버리는 순간이 좋다. </span>';
 	Nov16diary[4] 	= '<br><span>올해가 얼마 남지 않았다는 생각 때문인지 마음이 급하다. 파주에 오랜만에 왔으니 정리를 해야겠다.</span> ';
 
 	var Nov17diary 	= [];
 	Nov17diary[0] 	= '<span>오늘은 진짜 많이 잤다. 꿈도 안 꾼 거 같다. 많이 잔 김에 낮잠도 잤다. 아예 밤을 새울 생각이다.</span> ';
-	Nov17diary[1] 	= '<br><span>나세가 차를 태워줘서 민정과 같이 저녁을 먹고 두성집으로 갔다. 괴로운 노래 듣기 배틀이 벌어졌다. 나세는 음악을 오래 공부해서 징그러운 케이팝 듣기를 힘들어한다.</span> ';	
-	Nov17diary[2] 	= '<span>우리는 숨듣명에 대해서 생각해봤다. 왜 숨을까? 왜 들을까? 왜 명곡일까? 일단 우리가 그 노래들의 가사를 다 알고 있었다는 점이 아이러니하다. </span>';	
+	Nov17diary[1] 	= '<br><span>나세가 차를 태워줘서 민정과 같이 저녁을 먹고 두성집으로 갔다. 괴로운 노래 듣기 배틀이 벌어졌다. 나세는 음악을 오래 공부해서 징그러운 케이팝 듣기를 힘들어한다.</span> ';
+	Nov17diary[2] 	= '<span>우리는 숨듣명에 대해서 생각해봤다. 왜 숨을까? 왜 들을까? 왜 명곡일까? 일단 우리가 그 노래들의 가사를 다 알고 있었다는 점이 아이러니하다. </span>';
 	Nov17diary[3] 	= '<span class="questions" id="thisWork"><br>작업은 생각보다 아주 많이 길어져서, 아침 수업까지 잠깐이라도 잘 시간이 없었다. 분명 2-3시간을 예상했는데 거의 10시간 넘게 마우스를 만지작거렸다.</span> ';
 	Nov17diary[4] 	= '<span class="questions" id="work">물론 시간이 중요한 것은 아니고 마음에 들어야 하는데 그렇지가 않아서 좀 답답했다.</span> ';
 
 	var Nov18diary 	= [];
-	Nov18diary[0] 	= '<span class="questions" id="diary">오늘은 일주일을 기록하는 마지막 날이다. 나는 한동안 이 일주일을 곱씹게 될 것이다. 아마 기록한 것들로 괴로울 거고, 기록하지 못한 것들로 괴로울 거다. 시시한 글이지만 복잡했던 2020년에 대한 기록이 남게 되어 다행이다.</span> ';	
+	Nov18diary[0] 	= '<span class="questions" id="diary">오늘은 일주일을 기록하는 마지막 날이다. 나는 한동안 이 일주일을 곱씹게 될 것이다. 아마 기록한 것들로 괴로울 거고, 기록하지 못한 것들로 괴로울 거다. 시시한 글이지만 복잡했던 2020년에 대한 기록이 남게 되어 다행이다.</span> ';
 	Nov18diary[1] 	= '<br><img img class="diaryImg" src="assets/img/toy1.jpg"><span class="questions" id="toy">책장을 봤는데 장난감들이 다 넘어져 있다. 왜지? 아까 이불 들었을 때 닿았나. 세우기 귀찮아서 일단 그냥 뒀다. 전에 살던 집에서는 넘어진 장난감을 세워도 매번 다시 넘어졌다. 한동안 넘어진 장난감 세우기는 매일의 수행이 됐다.</span> ';
-	Nov18diary[2] 	= '<br><span class="questions" id="name">새로 할 작업을 위한 이름을 생각해봤다. 전에 생각해둔 단어인데 지금 꺼내면 되겠다는 생각이 들었다.</span> ';	
-	Nov18diary[3] 	= '<span class="questions" id="newWork">아직 정리 못 한 작업들이 많은데 새 작업을 하는 게 괜찮을지 모르겠다.</span> ';	
+	Nov18diary[2] 	= '<br><span class="questions" id="name">새로 할 작업을 위한 이름을 생각해봤다. 전에 생각해둔 단어인데 지금 꺼내면 되겠다는 생각이 들었다.</span> ';
+	Nov18diary[3] 	= '<span class="questions" id="newWork">아직 정리 못 한 작업들이 많은데 새 작업을 하는 게 괜찮을지 모르겠다.</span> ';
 	Nov18diary[4] 	= '<br><span>이제 꽤 추운데 눈은 언제 올까. 왠지 올해 눈이 엄청 많이 올 거 같다. 파주의 겨울이 어떨지 궁금하다. 일단 겨울에 가까워지니 <span id="star">별</span>이 잘 보여서 좋다. </span>';
 
 	var index12 = -1;
@@ -161,7 +164,7 @@ $(document).ready(function(){
 	$('.Nov14Date').click(function(){
 		index14++;
 		index14 < Nov14diary.length;
-		$('.diary-wrapper').append(Nov14diary[index14]);	
+		$('.diary-wrapper').append(Nov14diary[index14]);
 	});
 
 	$('.Nov15Date').click(function(){
@@ -238,7 +241,7 @@ $(document).ready(function(){
 				'color' : 'black'});
 			$('.info').css("width", "190px");
 		}else{
-		};	
+		};
 	});
 
 	$(document).on('click', '#star', function() {
@@ -336,7 +339,7 @@ $(document).ready(function(){
 		var leftX = $('#swimsuit-xray').offset().left;
 
 		$('#work-swimsuit-xray').css({
-		'top' : topY + 35,
+		'top' : topY+20,
 		'left' : leftX
 	});
 		$('.work-list').not('#work-xray').hide();
@@ -500,7 +503,7 @@ $(document).ready(function(){
 		$(function(){
 		$(".interview-list").draggable();
 	});
-		
+
 		$(document).on('click', '#exhibitionmap', function() {
 		$('#interview-exhibitionmap').animate({scrollTop: 0});
 		$(this).attr('class', 'questions-visited');
@@ -592,7 +595,7 @@ $(document).ready(function(){
 			top : 25
 		});
 		$('.interview-list').not('#interview-thisWork').css('z-index', '9');
-		$('#interview-thisWork').css('z-index', '10');		
+		$('#interview-thisWork').css('z-index', '10');
 	});
 
 	$(document).on('click', '#toy', function() {
